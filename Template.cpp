7832464c2355996a586cd5bc98fa47d6b05fa988
Tarikul3639 <123243130+Tarikul3639@users.noreply.github.com>
2023-10-21 21:08:15 +0600

    Add files via upload

---
+++ Template.cpp
@@ -0,0 +1,30 @@
+/*##################################################################
+ #                   Bismillahir Rahmanir Rahim                    #
+ #                         ALLAHU AKBAR                            #
+ #                                                                 #
+ #                  ***Author : TARIKUL ISLAM                      #
+ #        Bangladesh University of Business and Technology,(BUBT)  #
+ #                      ** Dept.of CSE.                            #
+###################################################################*/
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nn '\n'
+#define EPS 1e-9
+#define PI 3.1415926535897932384626433832795
+#define pb push_back
+#define asort(a) sort(a.begin(), a.end())
+#define areverse(a) reverse(a.begin(), a.end())
+#define forn(i, n) for (int i = 0; i < int(n); i++)
+#define sp " "
+#define test  \
+    ll t;     \
+    cin >> t; \
+    while (t--)
+#define removeDublicate(v) v.erase(unique(v.begin(), v.end()), v.end());
+
+int main()
+{
+    return 0;
+}
